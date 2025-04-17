c063eadf654257289e53723ca79fc77885f06f91
bom0320 <s24040@gsm.hs.kr>
2025-04-17 17:55:12 +0900

    fix: movies page

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,22 +1,15 @@
 import { API_URL } from "../../../(Home)/page";
 
-
 async function getMovie(id: string) {
-    console.log(`Fetching movies: ${Date.now()}`);
     const response = await fetch(`${API_URL}/${id}`);
     return response.json();
 }
 
 async function getVideos(id: string) {
-    console.log(`Fetching Videos: ${Date.now()}`);
     const response = await fetch(`${API_URL}/${id}/videos`)
     return response.json();
 }
-
-export default async function MovieDetail({ params: { id } }: { params: { id: string } }) {
-    console.log("==========");
-    console.log("start fetching");
+export default async function MovieDetail({ params: { id } }) {
     const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
-    console.log("end fetching");
     return <h1>{movie.title}</h1>
 }
