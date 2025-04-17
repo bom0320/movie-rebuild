c327d14e9ca8b2ce37ef3c6f95cf38181ef485eb
bom0320 <s24040@gsm.hs.kr>
2025-04-17 19:55:52 +0900

    Sort folders into movie-info and movie-videos

---
+++ components/movie-info.tsx
@@ -0,0 +1,11 @@
+import { API_URL } from "../app/(Home)/page";
+
+async function getMovie(id: string) {
+    const response = await fetch(`${API_URL}/${id}`)
+    return response.json();
+}
+
+export default async function MovieInfo({ id }: { id: string }) {
+    const movies = await getMovie(id);
+    return <div>{JSON.stringify(movies)}</div>
+}
---
+++ components/movie-videos.tsx
@@ -0,0 +1,12 @@
+import { API_URL } from "../app/(Home)/page";
+
+async function getVideos(id: string) {
+    const response = await fetch(`${API_URL}/${id}/videos`);
+    return response.json();
+}
+
+export default async function MovieVideos({ id }: { id: string }) {
+    const videos = await getVideos(id);
+    return <div>{JSON.stringify(videos)}</div>
+}
+
