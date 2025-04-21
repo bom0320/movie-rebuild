e73337cb055f7191c19190c854b382ea27079c2c
bom0320 <s24040@gsm.hs.kr>
2025-04-21 19:50:10 +0900

    movie-info

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,4 +1,5 @@
 import { API_URL } from "../app/(Home)/page";
+import styles from "../styles/movie-info.module.css";
 
 async function getMovie(id: string) {
     const response = await fetch(`${API_URL}/${id}`)
@@ -6,6 +7,15 @@ async function getMovie(id: string) {
 }
 
 export default async function MovieInfo({ id }: { id: string }) {
-    const movies = await getMovie(id);
-    return <div>{JSON.stringify(movies)}</div>
+    const movie = await getMovie(id);
+    return <div className={styles.container}>
+        <img src={movie.poster_path} alt={movie.title} />
+        <div className={styles.info}>
+            <h1 className={styles.movie_title}>{movie.title}</h1>
+            <h2>⭐️ {movie.vote_average}</h2>
+            <p>{movie.overview}</p>
+            <a href={movie.homepage} target={"_blank"}>Home &rarr;</a>
+        </div>
+
+    </div>
 }
