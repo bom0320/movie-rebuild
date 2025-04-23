0f0fb555b6a1aefa6de37b5653c1f8a7bd5ee65a
bom0320 <s24040@gsm.hs.kr>
2025-04-23 14:19:56 +0900

    move-info header UI

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -8,14 +8,20 @@ async function getMovie(id: string) {
 
 export default async function MovieInfo({ id }: { id: string }) {
     const movie = await getMovie(id);
-    return <div className={styles.container}>
-        <img src={movie.poster_path} alt={movie.title} />
-        <div className={styles.info}>
-            <h1 className={styles.movie_title}>{movie.title}</h1>
-            <h2>⭐️ {movie.vote_average}</h2>
-            <p>{movie.overview}</p>
-            <a href={movie.homepage} target={"_blank"}>Home &rarr;</a>
+    return <>
+        <div className={styles.container_main}>
+            <h1 className={styles.main_h1}>{movie.title}</h1>
+            <h2 className={styles.main_h2}>Information</h2>
         </div>
+        <div className={styles.container}>
+            <img src={movie.poster_path} alt={movie.title} className={styles.poster} />
+            <div className={styles.info}>
+                <h1 className={styles.movie_title}>{movie.title}</h1>
+                <h2>⭐️ {movie.vote_average}</h2>
+                <p>{movie.overview}</p>
+                <a href={movie.homepage} target={"_blank"}>Home &rarr;</a>
+            </div>
 
-    </div>
+        </div>
+    </>
 }
